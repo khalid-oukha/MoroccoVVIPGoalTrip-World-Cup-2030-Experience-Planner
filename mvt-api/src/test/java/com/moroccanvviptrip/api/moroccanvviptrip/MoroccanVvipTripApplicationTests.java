e5beb7b783a6fa43dea51b81038c913a0b186aab
khalidwaheli <oukhakhalid@gmail.com>
2025-01-08 23:33:59 +0100

    MOR-4 : As a developer, I want to set up Docker Compose to manage services (PostgreSQL, Redis, etc.) so that I can run the application locally with minimal effort.

--- mvt-api/src/test/java/com/moroccanvviptrip/api/moroccanvviptrip/MoroccanVvipTripApplicationTests.java
+++ mvt-api/src/test/java/com/moroccanvviptrip/api/moroccanvviptrip/MoroccanVvipTripApplicationTests.java
@@ -2,12 +2,13 @@ package com.moroccanvviptrip.api.moroccanvviptrip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MoroccanVvipTripApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
