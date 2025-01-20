a27b1f6ac9b07833d5674a01afe2876748b8cd3b
khalidwaheli <oukhakhalid@gmail.com>
2025-01-20 11:07:30 +0100

    @ControllerAdvice

---
+++ mvt-api/src/main/java/com/moroccanvviptrip/api/mvtapi/aop/SecurityExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.moroccanvviptrip.api.mvtapi.aop;
+
+import com.moroccanvviptrip.api.mvtapi.web.response.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
+
+@ControllerAdvice
+public class SecurityExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiErrorResponse> handleAccessDeniedException(AccessDeniedException ex) {
+        ApiErrorResponse response = ApiErrorResponse.create(
+                HttpStatus.UNAUTHORIZED.value(),
+                "Access denied: " + ex.getMessage(),
+                null
+        );
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+    }
+}
