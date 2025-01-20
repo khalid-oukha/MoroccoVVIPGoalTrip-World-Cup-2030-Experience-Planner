74214c678577019746c1b4828ab84868c43e807e
khalidwaheli <oukhakhalid@gmail.com>
2025-01-20 15:26:10 +0100

    MOR-27 : As an admin, I want to update existing city details so that I can modify information like descriptions or regions as needed.

---
+++ mvt-api/src/main/java/com/moroccanvviptrip/api/mvtapi/web/rest/CityController.java
@@ -0,0 +1,31 @@
+package com.moroccanvviptrip.api.mvtapi.web.rest;
+
+
+import com.moroccanvviptrip.api.mvtapi.domain.City;
+import com.moroccanvviptrip.api.mvtapi.services.CityService;
+import com.moroccanvviptrip.api.mvtapi.web.VM.CityResponseVm;
+import com.moroccanvviptrip.api.mvtapi.web.mapper.CityMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/city")
+@RequiredArgsConstructor
+public class CityController {
+
+    private final CityService cityService;
+    private final CityMapper cityMapper;
+
+    @GetMapping
+    public ResponseEntity<List<CityResponseVm>> getAll() {
+        List<City> cities = cityService.getAll();
+        List<CityResponseVm> responseVmList = cities.stream().map(cityMapper::toResponse).toList();
+        return ResponseEntity.ok(responseVmList);
+    }
+
+}
