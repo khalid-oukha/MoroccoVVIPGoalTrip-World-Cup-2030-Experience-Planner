c8700f60243120517c1cfa136ead37fc377dd8e1
khalidwaheli <oukhakhalid@gmail.com>
2025-01-29 18:20:26 +0100

    as front end developper i want to setup the project

---
+++ mvp-web-angular/src/app/modules/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import {AuthComponent} from './auth.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import {AngularSvgIconModule, SvgIconComponent} from 'angular-svg-icon';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AuthComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    AngularSvgIconModule.forRoot(),
+    NgOptimizedImage
+  ],
+  providers: [
+    [provideHttpClient(withInterceptorsFromDi())]
+  ]
+})
+export class AuthModule { }
